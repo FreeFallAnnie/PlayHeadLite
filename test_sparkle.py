52bfe6e660e407ac56a3c23cd71e97b6e0ed79e3
FreeFallAnnie <110285600+FreeFallAnnie@users.noreply.github.com>
2025-06-02 15:52:46 +1000

    Update test_sparkle.py
    update uart

--- test_sparkle.py
+++ test_sparkle.py
@@ -11,7 +11,7 @@ from bleak.backends.scanner import AdvertisementData
 
 DEVICE_NAME = "CIRCUITPY8167"  # matches what your CPB advertises
 UART_RX_CHAR_UUID = "6E400002-B5A3-F393-E0A9-E50E24DCCA9E"
-UART_SERVICE_UUID = "6E400001-B5A3-F393-E0A9-E50E24DCCA9E"
+UART_SERVICE_UUID = "6E400001-B5A3-F393-E0A9-E50E24DCCA9E"  # <- Add this!
 
 
 # BLE: Send a color command
